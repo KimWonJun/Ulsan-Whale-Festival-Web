02e455e7771d4d42453c55ac4ac6f866c41a72ae
WonJunKim <kimjnsjwj@gmail.com>
2019-06-02 16:30:18 +0900

    Add pins on map

--- script/script.js
+++ script/script.js
@@ -1,42 +1,70 @@
-const colors = [
-  '#5256FF',
-  '#4A78E8',
-  '#5EB6FF',
-  '#4ACBE8',
-  '#52FFF1',
-  '#94E0FF',
-  '#87B2E8',
-  '#94FFE5',
-  '#87E4E8',
-  '#A1E3FF',
-  '#87B1E8'
-];
+const colors = {
+  '밍크고래팀': '#ff3a3a',
+  '혹등고래팀': '#3a5bff',
+  '대왕고래팀': '#29ab5d',
+  '기본': '#6e6d6d',
+};
+
+const pins = {
+  '밍크고래팀': 'red',
+  '혹등고래팀': 'blue',
+  '대왕고래팀': 'green',
+  '기본': 'default',
+}
+
+function getTeamElem(team) {
+  const teamElem = document.createElement('li');
+  const circleElem = document.createElement('span');
+  const nameElem = document.createElement('span');
+  
+  circleElem.classList = 'circle';
+  circleElem.style.color = colors[team.name];
+  circleElem.innerText = '●';
+  nameElem.innerText = team.name;
+  
+  teamElem.appendChild(circleElem);
+  teamElem.appendChild(nameElem);
+
+  return teamElem;
+}
+
+function getMarkerElem(team, booth, left, top) {
+  const marker = document.createElement('div');
+  const name = document.createElement('span');
+  const pin = document.createElement('img');
+
+  marker.classList.add('marker');
+  marker.style.left = `${left}px`;
+  marker.style.top = `${top}px`;
+
+  name.classList.add('booth');
+  name.textContent = booth;
+
+  pin.classList.add('pin');
+  pin.src = `assets/pin_${pins[team || 'default']}.png`;
+
+  marker.appendChild(name);
+  marker.appendChild(pin);
+
+  return marker;
+}
 
 fetch('http://whale.istruly.sexy:1234/team').then(async res => {
   const result = await res.json();
   const list = document.querySelector('.team-list > ul');
-  console.log(result);
-  
-  result.forEach((e, i) => {
-    const team = document.createElement('li');
-    const circle = document.createElement('span');
-    const name = document.createElement('span');
-    
-    circle.classList = 'circle';
-    circle.style.color = colors[i];
-    circle.innerText = '●';
-    name.innerText = e.name;
-    
-    team.appendChild(circle);
-    team.appendChild(name);
-    list.appendChild(team);
-  });
+
+  result.forEach(team => list.appendChild(getTeamElem(team)));
 });
 
 // TODO: map 추가하고 핀 찍기
 fetch('http://whale.istruly.sexy:1234/map/web')
   .then(async res => {
     const result = await res.json();
-    if (res.status % 100 === 2)
-      document.querySelector('.remaining').textContent = result.leftTime;
+    if (res.status / 100 !== 2) return;
+    const map = document.querySelector('.map-container');
+
+    document.querySelector('.remaining').textContent = `${new Date(result.endTimeTimestamp).toLocaleString()}까지`;
+    result.map.forEach(booth => {
+      map.appendChild(getMarkerElem(booth.own_team, booth.booth_name, booth.x, booth.y));
+    });
   });
